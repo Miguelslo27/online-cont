d3ab0016db1ee13864772f99bfc3ad00fcb43914
Miguelslo27 <miguel.sosa@globant.com>
2018-04-09 19:19:17 -0300

    First component mi-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { MiHeaderComponent } from './components/mi-header/mi-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/mi-header/mi-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mi-header',
+  templateUrl: './mi-header.component.html',
+  styleUrls: ['./mi-header.component.css']
+})
+export class MiHeaderComponent {}
