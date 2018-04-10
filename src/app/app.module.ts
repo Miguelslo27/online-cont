b4160f5cf1402e52e2ccda6480f801874d1257b0
Juan García <juan.garcia@appsxxi.com>
2018-04-10 16:17:48 -0300

    Component Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MiHeaderComponent } from './components/mi-header/mi-header.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiHeaderComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule
